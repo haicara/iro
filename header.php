afbf3629e5c70f99b3e4632ea0ba3eb108e92239
Manabe Ayana <manabe.a@haicara,jp>
2020-03-11 22:52:05 +0900

    ドロワーメニュー改善

--- header.php
+++ header.php
@@ -10,23 +10,20 @@
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/drawer/3.2.2/css/drawer.min.css">
 <script src="https://cdnjs.cloudflare.com/ajax/libs/iScroll/5.2.0/iscroll.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/drawer/3.2.2/js/drawer.min.js"></script>
-<? /* Basic
+<? /* WordPress
 -------------------- */ ?>
 <?php wp_head(); ?>
 <link rel="stylesheet" href="<?php echo get_stylesheet_uri(); echo '?' . filemtime(get_stylesheet_directory() . '/style.css'); ?>" type="text/css" media="screen" />
 <? /* Branch
 -------------------- */ ?>
-<?php if ( is_home() || is_front_page() ) : ?>
-<?php else : ?>
 <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/js/highlight.css">
 <script src="<?php echo get_template_directory_uri(); ?>/js/highlight.pack.js"></script>
 <script>hljs.initHighlightingOnLoad();</script>
-<?php endif; ?>
 </head>
-<body class="drawer drawer--right">
+<body>
 <header>
     <div class="inner">
-		<h1 id="logo" role="banner">
+		<h1 class="logo" role="banner">
 			<?php // カスタムロゴを表示
 			if ( has_custom_logo() ) : ?>
 			<a href="<?php echo home_url(); ?>">
@@ -39,20 +36,28 @@
 			</a>
 			<?php endif ; ?>
 		</h1>
-		<button type="button" class="drawer-toggle drawer-hamburger">
-			<span class="sr-only">toggle navigation</span>
-			<span class="drawer-hamburger-icon"></span>
-		</button>
-		<nav class="drawer-nav" role="navigation">
-			<ul class="drawer-menu">
-				<li class="drawer-brand">MENU</li>
-				<?php wp_nav_menu( array(
-					'theme_location'=>'header_menu', 
-					'container'     =>'', 
-					'menu_class'    =>'',
-					'items_wrap'    =>'%3$s'));
-				?>
-			</ul>
+		<div class="l-drawer">
+		<input class="l-drawer__checkbox" id="drawerCheckbox" type="checkbox">
+		<!-- ドロワーアイコン -->
+		<label class="l-drawer__icon" for="drawerCheckbox">
+		  <span class="l-drawer__icon-parts"></span>
+		</label>
+		<!-- 背景を暗く -->
+		<label class="l-drawer__overlay" for="drawerCheckbox"></label>
+		<!-- ドロワーメニュー -->
+		<nav>
+		  <ul>
+			<li>
+			  <a href="/">メニュー</a>
+			</li>
+			<li>
+			  <a href="/">メニュー</a>
+			</li>
+			<li>
+			  <a href="/">メニュー</a>
+			</li>
+		  </ul>
 		</nav>
+		</div>
     </div>
 </header>
--- index.php
+++ index.php
@@ -1,24 +1,26 @@
 <?php get_header(); ?>
 <main>
 	<section>
-		<?php if ( have_posts() ) : ?>
-		<ul class="post-list">
-			<?php while ( have_posts() ) : the_post(); ?>
-			<li>
-				<a href="<?php the_permalink(); ?>">
-					<div class="post-list-thumb">
-						<?php the_post_thumbnail('medium'); ?>
-					</div>
-					<div class="post-list-desc">
-						<p><?php $category = get_the_category(); echo $category[0]->cat_name; ?></p>
-						<span><?php the_time('Y.m.d'); ?></span>
-						<h3><?php the_title(); ?></p>
-					</div>
-				</a>
-			</li>
-			<?php endwhile ; ?>
-		</ul>
-		<?php endif ; ?>
+		<div class="inner">
+			<?php if ( have_posts() ) : ?>
+			<ul class="post-list">
+				<?php while ( have_posts() ) : the_post(); ?>
+				<li>
+					<a href="<?php the_permalink(); ?>">
+						<div class="thumbnail">
+							<?php the_post_thumbnail('large'); ?>
+						</div>
+						<div class="description">
+							<p><?php $category = get_the_category(); echo $category[0]->cat_name; ?></p>
+							<span><?php the_time('Y.m.d'); ?></span>
+							<h3><?php the_title(); ?></p>
+						</div>
+					</a>
+				</li>
+				<?php endwhile ; ?>
+			</ul>
+			<?php endif ; ?>
+		</div>
 	</section>
 </main>
 <?php get_footer(); ?>
