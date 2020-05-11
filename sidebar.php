69ad6d7ecce18d78c5dc40e9b83bc1c6e895b361
Manabe Ayana <manabe.a@haicara,jp>
2020-05-11 12:34:12 +0900

    [remove]  使用していないコードを削除

--- sidebar.php
+++ sidebar.php
@@ -5,15 +5,6 @@
         <?php endif; ?>
     <?php endif; ?>
 <?php endif; ?>
-<dl class="widget" id="ranking">
-    <dt class="widget">RANKING</dt>
-    <?php
-    setPostViews(get_the_ID()); query_posts('meta_key=post_views_count&orderby=meta_value_num&posts_per_page=5&order=DESC&range=monthly'); while(have_posts()) : the_post(); ?>
-    <dd class="ranking">
-        <a class="ranking_img" href="<?php echo get_permalink(); ?>"><?php if ( has_post_thumbnail() ) { the_post_thumbnail( 'thumbnail'); } ?></a><a class="ranking_title" href="<?php echo get_permalink(); ?>"><?php the_title(); ?></a>
-    </dd>
-    <?php endwhile; ?>
-</dl>
 <?php if ( is_active_sidebar( 'sidebar' ) ) : ?>
     <?php dynamic_sidebar( 'sidebar' ); ?>
 <?php endif; ?>
